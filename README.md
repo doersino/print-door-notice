f7a4adb355c0626e31b68ef847c2f4d2977d30d7
Noah Doersing <doersino@gmail.com>
2019-09-28 10:50:57 +0200

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # print-door-notice
+
+*Fire off a single command to print out a notice that's ready to be taped to a door, letting passers-by know about room changes or other matters.*
+
+For an example output, see [`door-notice.pdf`](door-notice.pdf).
+
+
+## Usage
+
+```
+./print-door-notice.sh [--print] HEADING [DETAILS]
+```
+
+If the `--print` flag is supplied, the script invokes the `lpr` utility, which will attempt to send the generated PDF to the default printer configured on your system. If the flag is missing, the generated PDF `door-sign.pdf` will end up in your current working directory.
+
+Requires `pdftex` and some packages that should come with most LaTeX distributions.
---
+++ print-door-notice.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "USAGE: ./print-door-notice.sh [--print] HEADING [DETAILS]" && exit
+# e.g. ./print-door-notice.sh "This office will not tolerate redundancy in this office." "Those tolerant of redundancy\footnote{Which is not tolerated in this office.} will be made redundant."
+
+CWD="$PWD"
+
+[ "$1" == "--print" ] && PRINT=true || PRINT=false
+
+HEADING="$1"
+DETAILS="$2"
+
+TMP="$(mktemp -d)"
+cat > "$TMP/door-notice.tex" <<LATEX
+\documentclass[extrafontsizes,36pt]{memoir}
+\usepackage[T1]{fontenc}
+\usepackage[a4paper,landscape,top=0cm]{geometry}
+%\usepackage{german}
+%\usepackage[babel,german=quotes]{csquotes}
+\usepackage{csquotes}
+\pagestyle{empty}
+
+\newcommand{\announce}[2]{
+  \vspace*{\fill}
+  \begin{center}
+    \large \textbf{#1} \\\\[1cm]
+    \oldfootnotesize #2
+  \end{center}
+  \vspace*{\fill}
+}
+
+\let\oldfootnotesize\footnotesize
+\renewcommand*{\footnotesize}{\oldfootnotesize\tiny}
+
+\begin{document}
+
+\announce{$HEADING}{$DETAILS}
+
+\end{document}
+LATEX
+
+cd "$TMP"
+pdflatex "$TMP/door-notice.tex"
+if [ "$PRINT" = true ]; then
+    lpr "$TMP/door-notice.pdf"
+else
+    cp "$TMP/door-notice.pdf" "$CWD/"
+    cd "$CWD"
+fi
